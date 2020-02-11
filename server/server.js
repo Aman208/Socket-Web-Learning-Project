9421b3e4d3cef494cd91e1b690fe17f4e186a662
Aman208 <amankumar20804@gmail.com>
2020-02-11 14:54:03 +0530

    simple socket.io setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketIO = require("socket.io");
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+const server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on('connection' , (socket) =>{
+  console.log("New user connected");
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+  });
+})
+
+
+ 
+
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
